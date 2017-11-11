45d514e21353be53ec6a0df5d1d1c1d4ea4a69a4
Abigail Banting <abanting00@gmail.com>
2017-11-11 02:37:48 -0500

    Added Pages Components and Style folder

---
+++ bbb-client/src/components/Competition.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Competition extends Component{
+	render(){
+		return(
+			<div>
+				<p>Testing Competition Component</p>
+			</div>
+		);
+	}
+};
+
+export default Competition;
---
+++ bbb-client/src/components/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component{
+	render(){
+		return(
+			<div>
+				<p>Testing About Component</p>
+			</div>
+		);
+	}
+};
+
+export default Test;
