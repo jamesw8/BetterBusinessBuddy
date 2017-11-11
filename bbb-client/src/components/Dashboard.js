9f9573f123b87c96f894b5168cfc6364fdf7fde3
Vincent Yu <vinjyu1@gmail.com>
2017-11-11 04:27:05 -0500

    basic bar graph of data

--- bbb-client/src/components/Dashboard.js
+++ bbb-client/src/components/Dashboard.js
@@ -11,3 +11,37 @@ class Dashboard extends Component{
 };
 
 export default Dashboard;
+
+var data = [
+	{"days": 1, "loans": 20},
+	{"days": 2, "loans": 10},
+	{"days": 3, "loans": 15},
+	{"days": 4, "loans": 11},
+	{"days": 5, "loans": 18},
+	{"days": 6, "loans": 23},
+	{"days": 7, "loans": 43},
+	{"days": 8, "loans": 55},
+	{"days": 9, "loans": 21},
+	{"days": 10, "loans": 35}];
+
+var width = 800
+var height = 600
+var color = "orange"
+var numOfData = data.length
+
+
+var container = d3.select("body")
+					.append("svg")
+					.attr("width", width)
+					.attr("height", height)
+					.style("border", "1px solid black")
+
+var days = container.selectAll("rect")
+					.data(data)
+					.enter()
+					.append("rect")
+					.attr("x", function(d) { return (width/numOfData)*(d.days-1) })
+					.attr("y", function(d) { return height-10*d.loans })
+					.attr("height", function(d) { return height })
+					.attr("width", width/numOfData)
+					.style("fill", color);
