8ea6a505f692dbacaca07c6dcdd9e0d1a6c9eeb9
Chris Panican <chrispanican@gmail.com>
2017-11-11 06:40:08 -0500

    Finished home page v2

--- bbb-client/src/components/Home.js
+++ bbb-client/src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../style/Home.css';
 
-class Home extends Component{
+class Home extends Component {
 	render(){
 		return(
 			<div>
@@ -10,23 +11,23 @@ class Home extends Component{
 						<h2>Better Business Buddy</h2>
 						<p>Helping you grow</p>
 						<hr />
-						<button className="btn">Get Started</button>
+						<Link to="/industry"> <button className="btn btn-success btn-lg">Get Started</button></Link>
 					</div>
 				</div>
 
 				<div className="container">
 					<div className="row texts">
-						<div className="col col-md-4">
+						<div className="col col-xm-12 col-md-4">
 							<i className="material-icons">location_on</i>
 							<h3>Find Your Location</h3>
 							<p>Enter your future business location and we will figure out how you would fare with the competition.</p>
 						</div>
-						<div className="col col-md-4">
+						<div className="col col-xm-12 col-md-4">
 							<i className="material-icons">trending_up</i>
 							<h3>Get Insights</h3>
 							<p>Our tool will ask for your credit score and other information to figure out what is the best for you.</p>
 						</div>
-						<div className="col col-md-4">
+						<div className="col col-xm-12 col-md-4">
 							<i className="material-icons">poll</i>
 							<h3>Plan Ahead</h3>
 							<p>With our interactive and easy-to-understand user interface, we can help save money with your future expenses.</p>
