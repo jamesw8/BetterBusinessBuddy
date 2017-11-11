057ecf009966a3613bf46139a68dbd7455abc880
jamesw8 <jameswong8@gmail.com>
2017-11-11 11:58:59 -0500

    Pie2

--- bbb-client/src/components/tempPie.js
+++ bbb-client/src/components/tempPie.js
@@ -28,7 +28,7 @@ class Pie extends Component {
 				</div>
 				<div id="pie1" className="container">
 					<div>
-						<h2>Operational Costs $455,100</h2>
+						<h2>Operational Costs $508,100</h2>
 					</div>
 					<PieChart
 					slices={[
@@ -55,14 +55,12 @@ class Pie extends Component {
 						]}
 					/>
 				</div>
-				<h1>Operational Costs for the first month</h1>
+				
 				<div id="legend2">
+				<h1 id="title2">Operational Costs for the following months</h1>
 					<ul>
-						<h1>One Time Cost</h1>
-						<li><h2><span id="greenTwo">&nbsp;&nbsp;&nbsp;&nbsp;</span> Kitchen Equipment $381,000</h2></li>
-						
 						<h1>Recurring Costs</h1>
-						<li><h2><span id="greenOne">&nbsp;&nbsp;&nbsp;&nbsp;</span> Land/Building/Furniture Costs $65,500</h2></li>
+						<li><h2><span id="greenOne">&nbsp;&nbsp;&nbsp;&nbsp;</span> Land/Building/Furniture Costs $15,500</h2></li>
 						<li><h2><span id="greenThree">&nbsp;&nbsp;&nbsp;&nbsp;</span> Food Supplies $8,100</h2></li>
 						<li><h2><span id="greenFour">&nbsp;&nbsp;&nbsp;&nbsp;</span> Labor $9,000</h2></li>
 						<li><h2><span id="greenFive">&nbsp;&nbsp;&nbsp;&nbsp;</span> Advertising/Marketing $44,500</h2></li>
@@ -70,17 +68,13 @@ class Pie extends Component {
 				</div>
 				<div id="pie2" className="container">
 					<div>
-						<h2>Operational Costs $455,100</h2>
+						<h2>Operational Costs $77,100</h2>
 					</div>
 					<PieChart
 					slices={[
 						{
 							color: '#596C56',
-							value: 65500,
-						},
-						{
-							color: '#435D36',
-							value: 381000,
+							value: 15500,
 						},
 						{
 							color: '#78AB46',
