5d9f294e3cd2bfc832ee7edfa0a9e78fc68b30ea
Abigail Banting <abanting00@gmail.com>
2017-11-11 06:51:48 -0500

    updated component files

--- bbb-client/src/index.js
+++ bbb-client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';  
 
 ReactDOM.render(
 	<BrowserRouter>
